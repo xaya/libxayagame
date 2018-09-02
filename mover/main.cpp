2fcbd5537c1f62c709fccbf91a025379e83304c2
Daniel Kraft <d@domob.eu>
2018-09-02 14:12:41 +0200

    Proper command-line flags for moverd.
    Use the gflags library to implement "proper" command-line flags for
    moverd.  Instead of a single positional argument for the JSON-RPC client
    connection and a hardcoded JSON-RPC server port, we now have two flags
    --xaya_rpc_url and --game_rpc_port.  Running the JSON-RPC server is now
    also optional (it is only started if an explicit non-zero port is given).

--- mover/main.cpp
+++ mover/main.cpp
@@ -1,3 +1,5 @@
+#include "config.h"
+
 #include "logic.hpp"
 
 #include "xayagame/game.hpp"
@@ -9,12 +11,18 @@
 
 #include <google/protobuf/stubs/common.h>
 
+#include <gflags/gflags.h>
 #include <glog/logging.h>
 
 #include <cstdlib>
+#include <iostream>
+#include <memory>
 
-/** Port for the game's RPC server.  */
-constexpr unsigned GAME_RPC_PORT = 29050;
+DEFINE_string (xaya_rpc_url, "",
+               "URL at which Xaya Core's JSON-RPC interface is available");
+DEFINE_int32 (game_rpc_port, 0,
+              "The port at which the game daemon's JSON-RPC server will be"
+              " start (if non-zero)");
 
 int
 main (int argc, char** argv)
@@ -22,9 +30,17 @@ main (int argc, char** argv)
   google::InitGoogleLogging (argv[0]);
   GOOGLE_PROTOBUF_VERIFY_VERSION;
 
-  CHECK_EQ (argc, 2) << "Usage: moverd JSON-RPC-URL";
+  gflags::SetUsageMessage ("Run Mover game daemon");
+  gflags::SetVersionString (PACKAGE_VERSION);
+  gflags::ParseCommandLineFlags (&argc, &argv, true);
 
-  const std::string jsonRpcUrl(argv[1]);
+  if (FLAGS_xaya_rpc_url.empty ())
+    {
+      std::cerr << "Error: --xaya_rpc_url must be set" << std::endl;
+      return EXIT_FAILURE;
+    }
+
+  const std::string jsonRpcUrl(FLAGS_xaya_rpc_url);
   jsonrpc::HttpClient httpConnector(jsonRpcUrl);
 
   xaya::Game game("mv");
@@ -37,12 +53,19 @@ main (int argc, char** argv)
   mover::MoverLogic rules;
   game.SetGameLogic (&rules);
 
-  jsonrpc::HttpServer httpServer(GAME_RPC_PORT);
-  xaya::GameRpcServer rpcServer(game, httpServer);
+  std::unique_ptr<jsonrpc::HttpServer> httpServer;
+  std::unique_ptr<xaya::GameRpcServer> rpcServer;
+  if (FLAGS_game_rpc_port != 0)
+    {
+      httpServer = std::make_unique<jsonrpc::HttpServer> (FLAGS_game_rpc_port);
+      rpcServer = std::make_unique<xaya::GameRpcServer> (game, *httpServer);
+    }
 
-  rpcServer.StartListening ();
+  if (rpcServer != nullptr)
+    rpcServer->StartListening ();
   game.Run ();
-  rpcServer.StopListening ();
+  if (rpcServer != nullptr)
+    rpcServer->StopListening ();
 
   google::protobuf::ShutdownProtobufLibrary ();
   return EXIT_SUCCESS;
