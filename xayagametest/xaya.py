5722a35370317011cf620183a6fdf956108fd550
Daniel Kraft <d@domob.eu>
2018-09-03 12:04:51 +0200

    Create Python wrapper for Xaya daemon instance.
    Start creating xayagametest, a Python library for running integration
    tests for Xaya games.  This commit adds a Python class that executes
    and handles a Xaya Core process in regtest mode, which will be one of
    the components of the final integration test.

---
+++ xayagametest/xaya.py
@@ -0,0 +1,86 @@
+# Copyright (C) 2018 The Xaya developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+"""
+Code for running the Xaya Core daemon as component in an integration test.
+"""
+
+import jsonrpclib
+import logging
+import os
+import os.path
+import shutil
+import subprocess
+import time
+
+
+RPC_PORT = 28100
+ZMQ_PORT = 28200
+
+
+class Node ():
+  """
+  An instance of the Xaya Core daemon that is running in regtest mode and
+  used as component in an integration test of a Xaya game.
+  """
+
+  def __init__ (self, basedir, binary):
+    self.log = logging.getLogger ("xayagametest.xayanode")
+    self.datadir = os.path.join (basedir, "xayanode")
+    self.binary = binary
+
+    self.config = {
+      "rpcuser": "xayagametest",
+      "rpcpassword": "xayagametest",
+      "rpcport": RPC_PORT,
+      "zmqpubgameblocks": "tcp://127.0.0.1:%d" % ZMQ_PORT
+    }
+    self.rpcurl = ("http://%s:%s@localhost:%d"
+        % (self.config["rpcuser"], self.config["rpcpassword"],
+           self.config["rpcport"]))
+
+    self.log.info ("Creating fresh data directory for Xaya node in %s"
+                    % self.datadir)
+    shutil.rmtree (self.datadir, ignore_errors=True)
+    os.mkdir (self.datadir)
+    with open (os.path.join (self.datadir, "xaya.conf"), "wt") as f:
+      f.write ("[regtest]\n")
+      for key, value in self.config.items ():
+        f.write ("%s=%s\n" % (key, value))
+
+    self.proc = None
+
+  def start (self):
+    if self.proc is not None:
+      self.log.error ("Xaya process is already running, not starting again")
+      return
+
+    self.log.info ("Starting new Xaya process")
+    args = [self.binary]
+    args.append ("-datadir=%s" % self.datadir)
+    args.append ("-noprinttoconsole")
+    args.append ("-regtest")
+    self.proc = subprocess.Popen (args)
+    self.rpc = jsonrpclib.Server (self.rpcurl)
+
+    self.log.info ("Waiting for the JSON-RPC server to be up...")
+    while True:
+      try:
+        data = self.rpc.getnetworkinfo ()
+        self.log.info ("Daemon %s is up" % data["subversion"])
+        break
+      except:
+        time.sleep (1)
+
+  def stop (self):
+    if self.proc is None:
+      self.log.error ("No Xaya process is running cannot stop it")
+      return
+
+    self.log.info ("Stopping Xaya process")
+    self.rpc.stop ()
+
+    self.log.info ("Waiting for Xaya process to stop...")
+    self.proc.wait ()
+    self.proc = None
