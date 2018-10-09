f6a8f473419c1382d33417690ca96fb76bf4566e
Daniel Kraft <d@domob.eu>
2018-10-09 17:55:14 +0200

    Implement SQLite-based storage.
    This adds an implementation of StorageInterface based on an SQLite
    database (and for that, it adds SQLite as a new dependency).
    
    Note that StorageInterface could be implemented with a no-SQL database
    instead, as only key lookups are required.  Hence, while this implements
    a persistent storage for https://github.com/xaya/libxayagame/issues/2,
    it is not meant to be used as general persistent storage (another one
    based on perhaps LMDB will be added for that use).  Instead, it should
    be used when games use SQLite anyway for their state, so that a single
    database holds everything and can be updated atomically (see
    https://github.com/xaya/libxayagame/issues/11).

---
+++ xayagame/sqlitestorage.hpp
@@ -0,0 +1,118 @@
+// Copyright (C) 2018 The Xaya developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef XAYAGAME_SQLITESTORAGE_HPP
+#define XAYAGAME_SQLITESTORAGE_HPP
+
+#include "storage.hpp"
+#include "uint256.hpp"
+
+#include <sqlite3.h>
+
+#include <map>
+#include <string>
+
+namespace xaya
+{
+
+/**
+ * Implementation of StorageInterface, where all data is stored in an SQLite
+ * database.  In general, a no-SQL database would be more suitable for game
+ * storage (as only key lookups are required), but this can be useful in
+ * combination with games that keep their game state in SQLite as well (so that
+ * a single database holds everything).
+ *
+ * The storage implementation here uses tables with prefix "xayagame_".
+ * Subclasses that wish to store custom other data must not use tables
+ * with this prefix.
+ */
+class SQLiteStorage : public StorageInterface
+{
+
+private:
+
+  /**
+   * The filename of the database.  This is needed for resetting the storage,
+   * which removes the file and reopens the database.
+   */
+  const std::string filename;
+
+  /** The SQLite database handle if the connection is open.  */
+  sqlite3* db = nullptr;
+
+  /**
+   * A cache of prepared statements (mapping from the SQL command to the
+   * statement pointer).
+   */
+  mutable std::map<std::string, sqlite3_stmt*> preparedStatements;
+
+  /**
+   * Opens the database at filename into the handle.  It is an error if the
+   * handle is already opened.
+   */
+  void OpenDatabase ();
+
+  /**
+   * Closes the internal database handle.  Assumes that the handle is open.
+   */
+  void CloseDatabase ();
+
+protected:
+
+  /**
+   * Sets up the database schema if it does not already exist.  This function is
+   * called after opening the database, including when it was first created (but
+   * not only then).  It creates the required tables if they do not yet exist.
+   *
+   * Subclasses can override the method to also set up their schema (and,
+   * perhaps, to upgrade the schema when the software version changes).  They
+   * should make sure to call the superclass method as well in their
+   * implementation.
+   */
+  virtual void SetupSchema ();
+
+  /**
+   * Prepares an SQL statement given as string and stores it in the cache,
+   * or retrieves the existing statement from the cache.  The prepared statement
+   * is also reset, so that it can be reused right away.
+   *
+   * The returned statement is managed (and, in particular, finalised) by the
+   * SQLiteStorage object, not by the caller.
+   */
+  sqlite3_stmt* PrepareStatement (const std::string& sql) const;
+
+public:
+
+  explicit SQLiteStorage (const std::string& f);
+
+  SQLiteStorage () = delete;
+  SQLiteStorage (const SQLiteStorage&) = delete;
+  void operator= (const SQLiteStorage&) = delete;
+
+  ~SQLiteStorage ();
+
+  /**
+   * Clears the storage.  This deletes and re-creates the full database,
+   * and does not only delete from the tables that SQLiteStorage itself uses.
+   * This ensures that all data, including about game states, that is stored
+   * in the same database is removed consistently.
+   */
+  void Clear () override;
+
+  bool GetCurrentBlockHash (uint256& hash) const override;
+  GameStateData GetCurrentGameState () const override;
+  void SetCurrentGameState (const uint256& hash,
+                            const GameStateData& data) override;
+
+  bool GetUndoData (const uint256& hash, UndoData& data) const override;
+  void AddUndoData (const uint256& hash,
+                    unsigned height, const UndoData& data) override;
+  void ReleaseUndoData (const uint256& hash) override;
+  void PruneUndoData (unsigned height) override;
+
+};
+
+} // namespace xaya
+
+#endif // XAYAGAME_SQLITESTORAGE_HPP
---
+++ xayagame/sqlitestorage_tests.cpp
@@ -0,0 +1,116 @@
+// Copyright (C) 2018 The Xaya developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "sqlitestorage.hpp"
+
+#include "storage_tests.hpp"
+
+#include <gtest/gtest.h>
+
+#include <cstdio>
+#include <memory>
+
+namespace xaya
+{
+namespace
+{
+
+/**
+ * Simple utility subclass of SQLiteStorage, which makes it default
+ * constructible with an in-memory database.
+ */
+class InMemorySQLiteStorage : public SQLiteStorage
+{
+
+public:
+
+  InMemorySQLiteStorage ()
+    : SQLiteStorage (":memory:")
+  {}
+
+};
+
+INSTANTIATE_TYPED_TEST_CASE_P (SQLite, BasicStorageTests,
+                               InMemorySQLiteStorage);
+INSTANTIATE_TYPED_TEST_CASE_P (SQLite, PruningStorageTests,
+                               InMemorySQLiteStorage);
+
+/**
+ * Tests for SQLiteStorage with a temporary on-disk database file (instead of
+ * just an in-memory database).  They verify explicitly that data is persisted
+ * for a second instantiation of SQLiteStorage that uses the same file.
+ */
+class PersistentSQLiteStorageTests : public testing::Test
+{
+
+protected:
+
+  /** Example uint256 value for use as block hash in the test.  */
+  uint256 hash;
+
+  /** Example game state value.  */
+  const GameStateData state = "some game state";
+
+  /** Example undo data value.  */
+  const UndoData undo = "some undo data";
+
+  /** Name of the temporary file used for the database.  */
+  std::string filename;
+
+  PersistentSQLiteStorageTests ()
+  {
+    CHECK (hash.FromHex ("99" + std::string (62, '0')));
+
+    filename = std::tmpnam (nullptr);
+    LOG (INFO) << "Using temporary database file: " << filename;
+  }
+
+  ~PersistentSQLiteStorageTests ()
+  {
+    LOG (INFO) << "Cleaning up temporary file: " << filename;
+    std::remove (filename.c_str ());
+  }
+
+};
+
+TEST_F (PersistentSQLiteStorageTests, PersistsData)
+{
+  {
+    SQLiteStorage storage(filename);
+    storage.SetCurrentGameState (hash, state);
+    storage.AddUndoData (hash, 42, undo);
+  }
+
+  {
+    SQLiteStorage storage(filename);
+
+    uint256 h;
+    ASSERT_TRUE (storage.GetCurrentBlockHash (h));
+    EXPECT_TRUE (h == hash);
+    EXPECT_EQ (storage.GetCurrentGameState (), state);
+
+    UndoData val;
+    ASSERT_TRUE (storage.GetUndoData (hash, val));
+    EXPECT_EQ (val, undo);
+  }
+}
+
+TEST_F (PersistentSQLiteStorageTests, ClearWithOnDiskFile)
+{
+  SQLiteStorage storage(filename);
+  storage.SetCurrentGameState (hash, state);
+  storage.AddUndoData (hash, 42, undo);
+
+  uint256 h;
+  UndoData val;
+  EXPECT_TRUE (storage.GetCurrentBlockHash (h));
+  EXPECT_TRUE (storage.GetUndoData (hash, val));
+
+  storage.Clear ();
+  EXPECT_FALSE (storage.GetCurrentBlockHash (h));
+  EXPECT_FALSE (storage.GetUndoData (hash, val));
+}
+
+} // anonymous namespace
+} // namespace xaya
