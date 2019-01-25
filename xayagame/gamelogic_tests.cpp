82b8300b1054aba24988d1e00afc9d402dd97cd6
Daniel Kraft <d@domob.eu>
2019-01-25 17:27:10 +0100

    Generalise test fixture of gamelogic_tests.
    Make the test fixture with "simulated blockchain" a template, so that it
    can be used with other game rule classes in the future.  This will be used
    for testing randomness from the Context in a next step.

--- xayagame/gamelogic_tests.cpp
+++ xayagame/gamelogic_tests.cpp
@@ -6,6 +6,8 @@
 
 #include "storage.hpp"
 
+#include "testutils.hpp"
+
 #include <json/json.h>
 
 #include <gtest/gtest.h>
@@ -18,6 +20,81 @@ namespace xaya
 {
 namespace
 {
+
+constexpr const char GAME_ID[] = "test game";
+
+/**
+ * Fixture class with a simulated blockchain for verifying basic working
+ * of GameLogic instances without the need to have a real Game instance.
+ */
+template <typename G>
+  class GameLogicFixture : public testing::Test
+{
+
+protected:
+
+  G game;
+
+  /** The current game state in the simulated blockchain.  */
+  GameStateData state;
+  /** The stack of block data that has been attached.  */
+  std::stack<Json::Value> blockStack;
+  /** The stack of undo data for the simulated blockchain.  */
+  std::stack<UndoData> undoStack;
+
+  GameLogicFixture ()
+  {
+    game.SetGameId (GAME_ID);
+
+    unsigned dummyHeight;
+    std::string dummyHashHex;
+    state = game.GetInitialState (dummyHeight, dummyHashHex);
+  }
+
+  /**
+   * Processes the state forward using game and the simulated blockchain.
+   */
+  void
+  AttachBlock (const Json::Value& moves)
+  {
+    Json::Value blk(Json::objectValue);
+    blk["rngseed"] = BlockHash (blockStack.size ()).ToHex ();
+
+    Json::Value blockData(Json::objectValue);
+    blockData["block"] = blk;
+    blockData["moves"] = moves;
+
+    blockStack.push (blockData);
+
+    UndoData undo;
+    state = game.ProcessForward (state, blockData, undo);
+    undoStack.push (undo);
+  }
+
+  /**
+   * Processes the state backwards using game and our simulated blockchain.
+   */
+  void
+  DetachBlock ()
+  {
+    state = game.ProcessBackwards (state, blockStack.top (), undoStack.top ());
+
+    undoStack.pop ();
+    blockStack.pop ();
+  }
+
+  /**
+   * Constructs a moves array that has no actual data.
+   */
+  static Json::Value
+  NoMove ()
+  {
+    return Json::Value (Json::arrayValue);
+  }
+
+};
+
+/* ************************************************************************** */
 
 /**
  * A very simple game implemented using CachingGame:  The state is just a string
@@ -40,92 +117,29 @@ protected:
   GameStateData
   GetInitialStateInternal (unsigned& height, std::string& hashHex) override
   {
-    LOG (FATAL) << "This should not be called by the test";
+    return "";
   }
 
 };
 
-class CachingGameTests : public testing::Test
+class CachingGameTests : public GameLogicFixture<ReplacingGame>
 {
 
 protected:
 
-  ReplacingGame game;
-
-  /** The current game state in the simulated blockchain.  */
-  GameStateData state;
-  /** The stack of block data that has been attached.  */
-  std::stack<Json::Value> blockStack;
-  /** The stack of undo data for the simulated blockchain.  */
-  std::stack<UndoData> undoStack;
-
-  CachingGameTests ()
-  {
-    game.SetGameId ("replacing game");
-  }
-
   /**
-   * Processes the state forward using game and the simulated blockchain.
-   */
-  void
-  AttachBlock (const Json::Value& blockData)
-  {
-    blockStack.push (blockData);
-
-    UndoData undo;
-    state = game.ProcessForward (state, blockData, undo);
-    undoStack.push (undo);
-  }
-
-  /**
-   * Processes the state backwards using game and our simulated blockchain.
-   */
-  void
-  DetachBlock ()
-  {
-    state = game.ProcessBackwards (state, blockStack.top (), undoStack.top ());
-
-    undoStack.pop ();
-    blockStack.pop ();
-  }
-
-  /**
-   * Constructs blockdata that sets the given new string as "move".
+   * Constructs a move array that sets the given value.
    */
   static Json::Value
   Move (const std::string& value)
   {
-    Json::Value block(Json::objectValue);
-    block["rngseed"]
-        = "0000000000000000000000000000000000000000000000000000000000000001";
-
     Json::Value move(Json::objectValue);
     move["move"] = value;
 
     Json::Value moves(Json::arrayValue);
     moves.append (move);
 
-    Json::Value data(Json::objectValue);
-    data["block"] = block;
-    data["moves"] = moves;
-
-    return data;
-  }
-
-  /**
-   * Constructs blockdata that has no move (no change to state).
-   */
-  static Json::Value
-  NoMove ()
-  {
-    Json::Value block(Json::objectValue);
-    block["rngseed"]
-        = "0000000000000000000000000000000000000000000000000000000000000001";
-
-    Json::Value data(Json::objectValue);
-    data["block"] = block;
-    data["moves"] = Json::Value (Json::arrayValue);
-    return data;
+    return moves;
   }
 
 };
@@ -153,6 +167,8 @@ TEST_F (CachingGameTests, Works)
   EXPECT_TRUE (blockStack.empty ());
   EXPECT_EQ (state, "");
 }
+
+/* ************************************************************************** */
 
 } // anonymous namespace
 } // namespace xaya
