f9452a57cdc062478af42f57fa45e30925c1cfc0
Daniel Kraft <d@domob.eu>
2019-01-25 14:03:20 +0100

    Add Random utility class.
    The new Random class encapsulates a 256-bit seed and produces deterministic
    pseudo-random numbers based from it.  This is done by re-hashing the seed
    with SHA-256 every time it runs out and more bytes are required.

---
+++ xayagame/random_tests.cpp
@@ -0,0 +1,68 @@
+// Copyright (C) 2019 The Xaya developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "random.hpp"
+
+#include <gtest/gtest.h>
+
+#include <glog/logging.h>
+
+namespace xaya
+{
+namespace
+{
+
+/** The seed (as hex string) that we use in tests.  */
+constexpr const char SEED[]
+    = "7ca22c1665349f6c2cf40c7f7923e18184bbf3baa2b4096bee511b7a7eaf87e8";
+
+/**
+ * The test fixture class for Random seeds the instance with a predetermined
+ * seed.  We then also expect that the resulting random numbers match exactly
+ * given golden values.  This is so that we notice any changes that affect
+ * the generation of random numbers, as those changes would be consensus
+ * critical for any games relying on Random.
+ */
+class RandomTests : public testing::Test
+{
+
+protected:
+
+  Random rnd;
+
+  RandomTests ()
+  {
+    uint256 seed;
+    CHECK (seed.FromHex (SEED));
+    rnd.Seed (seed);
+  }
+
+};
+
+TEST_F (RandomTests, Bytes)
+{
+  const uint8_t expectedBytes[] = {
+    /* This is the seed itself.  */
+    0x7c, 0xa2, 0x2c, 0x16, 0x65, 0x34, 0x9f, 0x6c,
+    0x2c, 0xf4, 0x0c, 0x7f, 0x79, 0x23, 0xe1, 0x81,
+    0x84, 0xbb, 0xf3, 0xba, 0xa2, 0xb4, 0x09, 0x6b,
+    0xee, 0x51, 0x1b, 0x7a, 0x7e, 0xaf, 0x87, 0xe8,
+
+    /* Some following bytes based on correct "re-seeding".  */
+    0x67, 0xd8, 0x11, 0xd6, 0x7f, 0xfb, 0x76, 0x45,
+  };
+
+  for (const auto b : expectedBytes)
+    ASSERT_EQ (rnd.Next<uint8_t> (), b);
+}
+
+TEST_F (RandomTests, Integers)
+{
+  ASSERT_EQ (rnd.Next<uint16_t> (), 0x7ca2);
+  ASSERT_EQ (rnd.Next<uint32_t> (), 0x2c166534);
+  ASSERT_EQ (rnd.Next<uint64_t> (), 0x9f6c2cf40c7f7923);
+}
+
+} // anonymous namespace
+} // namespace xaya
