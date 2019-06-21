d38bcd888002419a5a4b2bd929d2451584ef5041
Daniel Kraft <d@domob.eu>
2019-06-21 12:06:25 +0200

    Empty GSP for xayaships.
    This change adds a GSP executable for the Xayaships on-chain processing.
    For now, it does not do anything (except define current block heights
    as starting points).

---
+++ ships/logic.cpp
@@ -0,0 +1,73 @@
+// Copyright (C) 2019 The Xaya developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "logic.hpp"
+
+#include <glog/logging.h>
+
+namespace ships
+{
+
+const xaya::BoardRules&
+ShipsLogic::GetBoardRules () const
+{
+  return boardRules;
+}
+
+void
+ShipsLogic::SetupSchema (sqlite3* db)
+{
+  LOG (WARNING) << "Setting up the schema is not yet implemented";
+}
+
+void
+ShipsLogic::GetInitialStateBlock (unsigned& height, std::string& hashHex) const
+{
+  const xaya::Chain chain = GetChain ();
+  switch (chain)
+    {
+    case xaya::Chain::MAIN:
+      height = 930000;
+      hashHex
+          = "0b615b33ad3b4da22af2fe53553fbcc49ae00e27ae04c1d7e275c2a76d8f87d9";
+      break;
+
+    case xaya::Chain::TEST:
+      height = 40000;
+      hashHex
+          = "74240aba644be39551e74c52eb4ffe6b63d1453c7d4cd1f6cfdee30d55354394";
+      break;
+
+    case xaya::Chain::REGTEST:
+      height = 0;
+      hashHex
+          = "6f750b36d22f1dc3d0a6e483af45301022646dfc3b3ba2187865f5a7d6d83ab1";
+      break;
+
+    default:
+      LOG (FATAL) << "Invalid chain value: " << static_cast<int> (chain);
+    }
+}
+
+void
+ShipsLogic::InitialiseState (sqlite3* db)
+{
+  /* The game simply starts with an empty database.  No stats for any names
+     yet, and also no channels defined.  */
+}
+
+void
+ShipsLogic::UpdateState (sqlite3* db, const Json::Value& blockData)
+{
+  LOG (WARNING) << "Updating the state for blocks is not yet implemented";
+}
+
+Json::Value
+ShipsLogic::GetStateAsJson (sqlite3* db)
+{
+  LOG (WARNING) << "Returning empty JSON state for now";
+  return Json::Value ();
+}
+
+} // namespace ships
---
+++ ships/logic.hpp
@@ -0,0 +1,52 @@
+// Copyright (C) 2019 The Xaya developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef XAYASHIPS_LOGIC_HPP
+#define XAYASHIPS_LOGIC_HPP
+
+#include "board.hpp"
+
+#include <gamechannel/boardrules.hpp>
+#include <gamechannel/channelgame.hpp>
+
+#include <json/json.h>
+
+#include <sqlite3.h>
+
+#include <string>
+
+namespace ships
+{
+
+/**
+ * The main game logic for the on-chain part of Xayaships.  This takes care of
+ * the public game state (win/loss statistics for names), management of open
+ * channels and dispute processing.
+ */
+class ShipsLogic : public xaya::ChannelGame
+{
+
+private:
+
+  ShipsBoardRules boardRules;
+
+protected:
+
+  const xaya::BoardRules& GetBoardRules () const override;
+
+  void SetupSchema (sqlite3* db) override;
+
+  void GetInitialStateBlock (unsigned& height,
+                             std::string& hashHex) const override;
+  void InitialiseState (sqlite3* db) override;
+
+  void UpdateState (sqlite3* db, const Json::Value& blockData) override;
+
+  Json::Value GetStateAsJson (sqlite3* db) override;
+
+};
+
+} // namespace ships
+
+#endif // XAYASHIPS_LOGIC_HPP
