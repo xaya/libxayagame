08ffda79f0e8d506d2b45ab76547445be2c2330f
Daniel Kraft <d@domob.eu>
2019-06-26 14:20:06 +0200

    Ships test using a winner statement.
    Add another integration test for shipsd, which now closes a channel
    using signed winner statements.  This relies on the recently introduced
    Python modules for gamechannels, and it also enables Python compilation
    for the ships protocol buffers.

--- ships/gametest/shipstest.py
+++ ships/gametest/shipstest.py
@@ -7,6 +7,15 @@ from xayagametest.testcase import XayaGameTest
 import os
 import os.path
 
+from gamechannel import signatures
+from gamechannel.proto import stateproof_pb2
+
+from proto import winnerstatement_pb2
+
+from google.protobuf import text_format
+
+import base64
+
 
 class ShipsTest (XayaGameTest):
   """
@@ -19,3 +28,42 @@ class ShipsTest (XayaGameTest):
       top_builddir = "../.."
     shipsd = os.path.join (top_builddir, "ships", "shipsd")
     super (ShipsTest, self).__init__ ("xs", shipsd)
+
+  def openChannel (self, names, addresses):
+    """
+    Creates a channel and joins it, so that we end up with a fully set-up
+    channel and two participants.  names and addresses should be arrays of
+    length two each, giving the names / addresses for the participants.
+    Returns the ID of the created channel.
+    """
+
+    self.assertEqual (len (names), 2)
+    self.assertEqual (len (addresses), 2)
+    cid = self.sendMove (names[0], {"c": {"addr": addresses[0]}})
+    self.generate (1)
+    self.sendMove (names[1], {"j": {"id": cid, "addr": addresses[1]}})
+    self.generate (1)
+
+    state = self.getGameState ()
+    assert cid in state["channels"]
+
+    return cid
+
+  def getWinnerStatement (self, cid, winner):
+    """
+    Constructs a winner statement for the given winner index and signs it
+    by all players whose addresses we have.  The returned statement
+    is already serialised and base64-encoded, so it can be put into a move.
+    """
+
+    state = self.getGameState ()
+    channel = state["channels"][cid]
+
+    stmt = winnerstatement_pb2.WinnerStatement ()
+    stmt.winner = winner
+
+    data = stmt.SerializeToString ()
+    sgn = signatures.createForChannel (self.rpc.xaya, channel,
+                                       "winnerstatement", data)
+
+    return base64.b64encode (sgn.SerializeToString ())
