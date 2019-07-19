1ebdda533ac620cac6b9a721a15d6785e47f5170
Daniel Kraft <d@domob.eu>
2019-07-19 15:32:33 +0200

    Test reorgs in Xayaships channels.
    The new reorg.py integration test verifies what happens if reorgs happen
    on-chain while a channel is being played.  In particular, we check that
    it is fine if the channel creation gets reorged, what happens if another
    player joins due to a reorg and how the channel daemon handles reorgs of
    resolutions, dispute timeouts and winner statements.

---
+++ ships/channeltest/reorg.py
@@ -0,0 +1,224 @@
+#!/usr/bin/env python
+# Copyright (C) 2019 The Xaya developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+"""
+Tests how a game channel reacts to reorgs of important on-chain transactions
+(channel creation, join, resolutions, winner statements).
+"""
+
+from shipstest import ShipsTest
+
+
+class ReogTest (ShipsTest):
+
+  def run (self):
+    self.generate (110)
+
+    # Create a test channel with two participants.  Remember the block hashes
+    # where it was created and joined by the second one, so that we can
+    # later invalidate those.
+    self.mainLogger.info ("Creating test channel...")
+    channelId = self.sendMove ("foo", {"c": {
+      "addr": self.newSigningAddress (),
+    }})
+    self.generate (1)
+    createBlk = self.rpc.xaya.getbestblockhash ()
+    self.sendMove ("bar", {"j": {
+      "id": channelId,
+      "addr": self.newSigningAddress (),
+    }})
+    self.generate (1)
+    joinBlk = self.rpc.xaya.getbestblockhash ()
+
+    # Start up three channel daemons:  The two participants and
+    # a third one, which will join the channel later in a reorged
+    # alternate reality.
+    self.mainLogger.info ("Starting channel daemons...")
+    with self.runChannelDaemon (channelId, "foo") as foo, \
+         self.runChannelDaemon (channelId, "bar") as bar, \
+         self.runChannelDaemon (channelId, "baz") as baz:
+
+      daemons = [foo, bar, baz]
+
+      self.mainLogger.info ("Running initialisation sequence...")
+      pos = """
+        xxxx..xx
+        ........
+        xxx.xxx.
+        ........
+        xx.xx.xx
+        ........
+        ........
+        ........
+      """
+      foo.rpc._notify.setposition (pos)
+      bar.rpc._notify.setposition (pos)
+      baz.rpc._notify.setposition (pos)
+
+      # Play a couple of turns and save the resulting "original" state.
+      for c in range (8):
+        _, state = self.waitForPhase (daemons, ["shoot"])
+        turn = state["current"]["state"]["whoseturn"]
+        daemons[turn].rpc._notify.shoot (row=7, column=c)
+      _, originalState = self.waitForPhase (daemons, ["shoot"])
+
+      # Generate a couple of blocks to make sure this will be the longest
+      # chain in the end.
+      self.generate (10)
+
+      self.mainLogger.info ("Reorg to channel creation...")
+      self.rpc.xaya.invalidateblock (createBlk)
+      self.assertEqual (foo.getCurrentState ()["existsonchain"], False)
+      self.rpc.xaya.reconsiderblock (createBlk)
+      self.rpc.xaya.invalidateblock (joinBlk)
+      self.assertEqual (self.rpc.xaya.getbestblockhash (), createBlk)
+      state = foo.getCurrentState ()
+      self.assertEqual (state["existsonchain"], True)
+      self.assertEqual (state["current"]["state"]["parsed"]["phase"],
+                        "single participant")
+
+      self.mainLogger.info ("Alternate join...")
+      self.sendMove ("baz", {"j": {
+        "id": channelId,
+        "addr": self.newSigningAddress (),
+      }})
+      self.expectPendingMoves ("bar", [])
+      self.expectPendingMoves ("baz", ["j"])
+      self.generate (1)
+
+      # Make sure it is baz' turn.  If not, miss a shot with foo.
+      self.mainLogger.info ("Building alternate reality...")
+      _, state = self.waitForPhase (daemons, ["shoot"])
+      if state["current"]["state"]["whoseturn"] == 0:
+        foo.rpc._notify.shoot (row=6, column=0)
+        _, state = self.waitForPhase (daemons, ["shoot"])
+      self.assertEqual (state["current"]["state"]["whoseturn"], 1)
+
+      # Finish the game and let foo win.
+      baz.rpc._notify.revealposition ()
+      _, state = self.waitForPhase (daemons, ["finished"])
+      self.assertEqual (state["current"]["state"]["parsed"]["winner"], 0)
+      self.generate (1)
+      self.expectGameState ({
+        "channels": {},
+        "gamestats": {
+          "foo": {"won": 1, "lost": 0},
+          "baz": {"won": 0, "lost": 1},
+        },
+      })
+
+      self.mainLogger.info ("Restoring original state...")
+      self.rpc.xaya.reconsiderblock (joinBlk)
+      state = foo.getCurrentState ()
+      self.assertEqual (state["current"]["meta"],
+                        originalState["current"]["meta"])
+      self.assertEqual (state["current"]["state"]["parsed"],
+                        originalState["current"]["state"]["parsed"])
+
+      # Make sure it is foo's turn.
+      _, state = self.waitForPhase (daemons, ["shoot"])
+      if state["current"]["state"]["whoseturn"] == 1:
+        bar.rpc._notify.shoot (row=6, column=1)
+        _, state = self.waitForPhase (daemons, ["shoot"])
+      self.assertEqual (state["current"]["state"]["whoseturn"], 0)
+
+      # Test what happens if a resolution move gets reorged away.  Then the
+      # player who resolved should still make sure it gets into the chain
+      # again (since they obviously still know a better state).
+      self.mainLogger.info ("Reorg of a resolution move...")
+      bar.rpc._notify.filedispute ()
+      self.expectPendingMoves ("bar", ["d"])
+      self.generate (1)
+      state = foo.getCurrentState ()
+      self.assertEqual (state["dispute"], {
+        "whoseturn": 0,
+        "canresolve": False,
+        "height": self.rpc.xaya.getblockcount (),
+      })
+      foo.rpc._notify.shoot (row=0, column=0)
+      self.expectPendingMoves ("foo", ["r"])
+      self.generate (1)
+      resolutionBlk = self.rpc.xaya.getbestblockhash ()
+      state = foo.getCurrentState ()
+      assert "dispute" not in state
+
+      self.rpc.xaya.invalidateblock (resolutionBlk)
+      state = foo.getCurrentState ()
+      self.assertEqual (state["dispute"], {
+        "whoseturn": 0,
+        "canresolve": True,
+        "height": self.rpc.xaya.getblockcount (),
+      })
+
+      # Currently, the channel daemon always resends, even if the
+      # previous move is still in the mempool.  For fixing this, see
+      # https://github.com/xaya/libxayagame/issues/65.  Hence, we should
+      # get *two* resolution moves.  If this issue gets fixed at some point,
+      # we should do two tests:  When the original move remains in the
+      # mempool, then no additional one should be sent.  If it is not anymore
+      # (e.g. because of a long reorg), then a *new* one should be sent.
+      self.expectPendingMoves ("foo", ["r", "r"])
+      self.generate (1)
+      state = foo.getCurrentState ()
+      assert "dispute" not in state
+
+      self.mainLogger.info ("Letting the game end with a dispute...")
+      bar.rpc._notify.filedispute ()
+      self.expectPendingMoves ("bar", ["d"])
+      self.generate (11)
+      self.expectGameState ({
+        "channels": {},
+        "gamestats": {
+          "foo": {"won": 0, "lost": 1},
+          "bar": {"won": 1, "lost": 0},
+        },
+      })
+
+      self.mainLogger.info ("Detaching a block and ending the game normally...")
+      disputeTimeoutBlk = self.rpc.xaya.getbestblockhash ()
+      self.rpc.xaya.invalidateblock (disputeTimeoutBlk)
+      state = foo.getCurrentState ()
+      self.assertEqual (state["existsonchain"], True)
+
+      # We miss a shot with foo and reveal with bar, so that this time
+      # foo wins the channel (but ordinarily).
+      foo.rpc._notify.shoot (row=6, column=2)
+      self.expectPendingMoves ("foo", ["r"])
+      self.generate (1)
+      self.waitForPhase (daemons, ["shoot"])
+      bar.rpc._notify.revealposition ()
+      _, state = self.waitForPhase (daemons, ["finished"])
+      self.assertEqual (state["current"]["state"]["parsed"]["winner"], 0)
+      self.expectPendingMoves ("foo", ["w"])
+      self.generate (1)
+      self.expectGameState ({
+        "channels": {},
+        "gamestats": {
+          "foo": {"won": 1, "lost": 0},
+          "bar": {"won": 0, "lost": 1},
+        },
+      })
+
+      self.mainLogger.info ("Reorg and resending of winner statement...")
+      winnerStmtBlk = self.rpc.xaya.getbestblockhash ()
+      self.rpc.xaya.invalidateblock (winnerStmtBlk)
+      state = foo.getCurrentState ()
+      self.assertEqual (state["current"]["state"]["parsed"]["phase"],
+                        "finished")
+      self.assertEqual (state["current"]["state"]["parsed"]["winner"], 0)
+      # As above, we also resend the winner statement move at the moment.
+      self.expectPendingMoves ("foo", ["w", "w"])
+      self.generate (1)
+      self.expectGameState ({
+        "channels": {},
+        "gamestats": {
+          "foo": {"won": 1, "lost": 0},
+          "bar": {"won": 0, "lost": 1},
+        },
+      })
+
+
+if __name__ == "__main__":
+  ReogTest ().main ()
