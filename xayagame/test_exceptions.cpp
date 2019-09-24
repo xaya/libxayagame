df4519a66464cb2c25645b41dcea7a1c68d0831b
Daniel Kraft <d@domob.eu>
2019-09-24 11:00:44 +0200

    Test exception thrown from JSON-RPC server.
    Extend test_exceptions to cover also a situation where the exception
    is thrown by a JSON-RPC server and then caught through the JSON-RPC
    client that made the call.

--- xayagame/test_exceptions.cpp
+++ xayagame/test_exceptions.cpp
@@ -2,6 +2,13 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#include "rpc-stubs/testrpcclient.h"
+#include "rpc-stubs/testrpcserverstub.h"
+
+#include <jsonrpccpp/client/connectors/httpclient.h>
+#include <jsonrpccpp/common/exception.h>
+#include <jsonrpccpp/server/connectors/httpserver.h>
+
 #include <glog/logging.h>
 
 #include <stdexcept>
@@ -30,6 +37,52 @@ BasicThrow ()
     }
 }
 
+/**
+ * RPC server that implements the "test" interface and just throws an exception.
+ */
+class TestRpcServer : public TestRpcServerStub
+{
+
+public:
+
+  using TestRpcServerStub::TestRpcServerStub;
+
+  int
+  add (const int a, const int b) override
+  {
+    throw jsonrpc::JsonRpcException (a + b, "expected error");
+  }
+
+};
+
+/**
+ * Tests an exception thrown through libjson-rpc-cpp.
+ */
+void
+JsonRpcThrow ()
+{
+  LOG (INFO) << "Testing exceptions in JSON-RPC...";
+
+  jsonrpc::HttpServer httpServer(12345);
+  TestRpcServer rpcServer(httpServer);
+  rpcServer.StartListening ();
+
+  jsonrpc::HttpClient httpClient("http://localhost:12345");
+  TestRpcClient rpcClient(httpClient);
+
+  try
+    {
+      LOG (INFO) << "Making call that will throw...";
+      rpcClient.add (1, 2);
+      LOG (FATAL) << "Exception not thrown!";
+    }
+  catch (const jsonrpc::JsonRpcException& exc)
+    {
+      LOG (INFO) << "Caught exception: " << exc.what ();
+      CHECK_EQ (exc.GetCode (), 3);
+    }
+}
+
 } // anonymous namespace
 
 int
@@ -38,6 +91,7 @@ main (int argc, char** argv)
   google::InitGoogleLogging (argv[0]);
 
   BasicThrow ();
+  JsonRpcThrow ();
 
   return EXIT_SUCCESS;
 }
