55cbdf8ceda110b48b4bf254bfb9eb65c7b7d8a2
Daniel Kraft <d@domob.eu>
2020-10-23 11:07:16 +0200

    Unit tests for SQLite statement.
    Add basic unit tests for the SQLiteDatabase::Statement class, checking
    stepping, resetting, binding parameters and extracting column values
    with the various types, NULL and so on.

--- xayagame/sqlitestorage_tests.cpp
+++ xayagame/sqlitestorage_tests.cpp
@@ -7,10 +7,13 @@
 #include "storage_tests.hpp"
 #include "testutils.hpp"
 
+#include "xayautil/hash.hpp"
+
 #include <gtest/gtest.h>
 
 #include <atomic>
 #include <cstdio>
+#include <limits>
 #include <memory>
 #include <thread>
 
@@ -42,6 +45,175 @@ INSTANTIATE_TYPED_TEST_CASE_P (SQLite, PruningStorageTests,
                                InMemorySQLiteStorage);
 INSTANTIATE_TYPED_TEST_CASE_P (SQLite, TransactingStorageTests,
                                InMemorySQLiteStorage);
+
+/* ************************************************************************** */
+
+class SQLiteStatementTests : public testing::Test
+{
+
+protected:
+
+  SQLiteDatabase db;
+
+  SQLiteStatementTests ()
+    : db("foo", SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE | SQLITE_OPEN_MEMORY)
+  {
+    CHECK_EQ (sqlite3_exec (*db, R"(
+      CREATE TABLE `test`
+        (`int` INTEGER NULL,
+         `text` TEXT NULL,
+         `blob` BLOB NULL);
+    )", nullptr, nullptr, nullptr), SQLITE_OK);
+  }
+
+  /**
+   * Tests a "roundtrip" of storing a value into our test database
+   * with a prepared statement parameter bind, and then extracting
+   * it again to test that the binding and extraction works.
+   */
+  template <typename T>
+    void
+    Roundtrip (const std::string& column, const T& val)
+  {
+    auto stmt = db.Prepare (R"(
+      INSERT INTO `test`
+        (`)" + column + R"(`) VALUES (?1)
+    )");
+    stmt.Bind (1, val);
+    stmt.Execute ();
+
+    stmt = db.PrepareRo (R"(
+      SELECT `)" + column + R"(` FROM `test`
+    )");
+
+    ASSERT_TRUE (stmt.Step ());
+    EXPECT_EQ (stmt.Get<T> (0), val);
+    ASSERT_FALSE (stmt.Step ());
+
+    stmt = db.Prepare (R"(
+      DELETE FROM `test`
+    )");
+    stmt.Execute ();
+  }
+
+};
+
+TEST_F (SQLiteStatementTests, BindExtract)
+{
+  Roundtrip<int> ("int", 0);
+  Roundtrip<int> ("int", 123);
+  Roundtrip<int> ("int", -5);
+
+  Roundtrip<unsigned> ("int", 0);
+  Roundtrip<unsigned> ("int", 1 << 20);
+
+  Roundtrip<int64_t> ("int", 0);
+  Roundtrip<int64_t> ("int", std::numeric_limits<int64_t>::min ());
+  Roundtrip<int64_t> ("int", std::numeric_limits<int64_t>::max ());
+
+  Roundtrip<uint64_t> ("int", 0);
+  Roundtrip<uint64_t> ("int", std::numeric_limits<int64_t>::max ());
+
+  Roundtrip<bool> ("int", false);
+  Roundtrip<bool> ("int", true);
+
+  Roundtrip<std::string> ("text", "");
+  Roundtrip<std::string> ("text", "foobar");
+  Roundtrip<std::string> ("text", u8"äöü");
+  Roundtrip<std::string> ("text", std::string ("abc\0def", 7));
+
+  uint256 zero;
+  zero.SetNull ();
+  Roundtrip<uint256> ("blob", zero);
+  Roundtrip<uint256> ("blob", SHA256::Hash ("foobar"));
+}
+
+TEST_F (SQLiteStatementTests, BindCheckNull)
+{
+  auto stmt = db.Prepare (R"(
+    INSERT INTO `test`
+      (`int`, `text`) VALUES (1, ?1), (2, ?2)
+  )");
+  stmt.Bind<std::string> (1, "foo");
+  stmt.Bind<std::string> (2, "wrong");
+  stmt.BindNull (2);
+  stmt.Execute ();
+
+  stmt = db.PrepareRo (R"(
+    SELECT `text`
+      FROM `test`
+      ORDER BY `int`
+  )");
+  ASSERT_TRUE (stmt.Step ());
+  ASSERT_FALSE (stmt.IsNull (0));
+  EXPECT_EQ (stmt.Get<std::string> (0), "foo");
+  ASSERT_TRUE (stmt.Step ());
+  EXPECT_TRUE (stmt.IsNull (0));
+  ASSERT_FALSE (stmt.Step ());
+}
+
+TEST_F (SQLiteStatementTests, BindExtractBlob)
+{
+  std::string binary;
+  for (int i = 0; i <= 0xFF; ++i)
+    binary.push_back (static_cast<char> (i));
+
+  auto stmt = db.Prepare (R"(
+    INSERT INTO `test`
+      (`int`, `blob`) VALUES (1, ?1), (2, ?2)
+  )");
+  stmt.BindBlob (1, "");
+  stmt.BindBlob (2, binary);
+  stmt.Execute ();
+
+  stmt = db.PrepareRo (R"(
+    SELECT `blob`
+      FROM `test`
+      ORDER BY `int`
+  )");
+  ASSERT_TRUE (stmt.Step ());
+  EXPECT_EQ (stmt.GetBlob (0), "");
+  ASSERT_TRUE (stmt.Step ());
+  EXPECT_EQ (stmt.GetBlob (0), binary);
+  ASSERT_FALSE (stmt.Step ());
+}
+
+TEST_F (SQLiteStatementTests, Reset)
+{
+  auto stmt = db.Prepare (R"(
+    INSERT INTO `test`
+      (`int`, `text`)
+      VALUES
+        (1, 'foo'),
+        (1, 'bar'),
+        (2, 'baz')
+  )");
+  stmt.Execute ();
+
+  stmt = db.PrepareRo (R"(
+    SELECT `text`
+      FROM `test`
+      WHERE `int` = ?1
+      ORDER BY `text`
+  )");
+  stmt.Bind (1, 1);
+
+  for (int i = 0; i < 10; ++i)
+    {
+      stmt.Reset ();
+      ASSERT_TRUE (stmt.Step ());
+      EXPECT_EQ (stmt.Get<std::string> (0), "bar");
+      ASSERT_TRUE (stmt.Step ());
+      EXPECT_EQ (stmt.Get<std::string> (0), "foo");
+      ASSERT_FALSE (stmt.Step ());
+    }
+
+  stmt.Reset ();
+  stmt.Bind (1, 2);
+  ASSERT_TRUE (stmt.Step ());
+  EXPECT_EQ (stmt.Get<std::string> (0), "baz");
+  ASSERT_FALSE (stmt.Step ());
+}
 
 /* ************************************************************************** */
 
