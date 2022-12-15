fc873d60e37d6eff836275bd4a7e321d09b2f285
Daniel Kraft <d@domob.eu>
2022-12-15 08:27:02 +0100

    Add pending support to websocket Python script.
    This extends the Python websocket adapter script so that it optionally
    also supports tracking of pending updates.

--- websocket/gsp-websocket-server.py
+++ websocket/gsp-websocket-server.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright (C) 2021 The Xaya developers
+# Copyright (C) 2021-2022 The Xaya developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -16,13 +16,19 @@ from websocket_server import WebsocketServer
 
 class Server:
   """
-  A Websocket server that polls a GSP's waitforchange RPC interface and
-  pushes notifications to connected clients.
+  A Websocket server that polls a GSP's waitforchange and waitforpendingchange
+  RPC interfaces and pushes notifications to connected clients.
   """
 
   def __init__ (self, host, port, gspRpcUrl):
     self.srv = WebsocketServer (host=host, port=port, loglevel=logging.INFO)
-    self.rpc = jsonrpclib.ServerProxy (gspRpcUrl)
+    self.rpcUrl = gspRpcUrl
+    self.log = logging.Logger ("")
+    self.withPending = False
+
+  def enablePending (self):
+    self.withPending = True
+    self.log.info ("Pending moves enabled")
 
   def run (self):
     """
@@ -43,32 +49,61 @@ class Server:
     mut = threading.Lock ()
     shouldStop = False
 
-    def loop ():
-      knownBlock = ""
+    def loop (rpc, firstKnown, pusher):
+      known = firstKnown
       while True:
-        newBlock = self.rpc.waitforchange (knownBlock)
-        if newBlock != knownBlock:
-          self.pushNewBlock (newBlock)
-          knownBlock = newBlock
         with mut:
           if shouldStop:
             return
+        new = rpc (known)
+        if new != known:
+          pusher (new)
+          known = new
 
-    thread = threading.Thread (target=loop)
-    thread.start ()
+    threads = []
+
+    rpcBlocks = jsonrpclib.ServerProxy (self.rpcUrl)
+    threads.append (threading.Thread (target=loop, args=(
+        rpcBlocks.waitforchange, "", self.pushNewBlock
+    )))
+
+    if self.withPending:
+      rpcPending = jsonrpclib.ServerProxy (self.rpcUrl)
+      # Closure wrapping around the waitforpendingchange RPC, so that it
+      # accepts as argument a JSON object with version (rather than just the
+      # version), such as it returns.  This makes it fit into
+      # the same schema as waitforchange, and is all we need.
+      def waitForPending (data):
+        return rpcPending.waitforpendingchange (data["version"])
+      threads.append (threading.Thread (target=loop, args=(
+          waitForPending, {"version": 0}, self.pushPendingUpdate
+      )))
+
+    for t in threads:
+      t.start ()
 
     try:
       yield
     finally:
       with mut:
         shouldStop = True
-      thread.join ()
+      for t in threads:
+        t.join ()
 
   def pushNewBlock (self, blockHash):
     data = {
       "jsonrpc": "2.0",
       "method": "newblock",
       "params": [blockHash],
+    }
+    msg = json.dumps (data, separators=(",", ":"))
+    self.srv.send_message_to_all (msg)
+
+  def pushPendingUpdate (self, data):
+    data = {
+      "jsonrpc": "2.0",
+      "method": "pendingupdate",
+      "params": [data],
     }
     msg = json.dumps (data, separators=(",", ":"))
     self.srv.send_message_to_all (msg)
@@ -84,7 +119,11 @@ if __name__ == "__main__":
                        help="Host to listen on for Websocket connections")
   parser.add_argument ("--gsp_rpc_url", required=True,
                        help="URL for the GSP's JSON-RPC interface")
+  parser.add_argument ("--enable_pending", action="store_true",
+                       help="Also track and push updates to pending moves")
   args = parser.parse_args ()
 
   srv = Server (args.host, args.port, args.gsp_rpc_url)
+  if args.enable_pending:
+    srv.enablePending ()
   srv.run ()
